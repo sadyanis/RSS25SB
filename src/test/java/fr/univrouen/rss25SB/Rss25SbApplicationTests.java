1b46972a2c92b69d2f2c78e0ae15fda4ccb1bcd4
sadyanis <126914554+sadyanis@users.noreply.github.com>
2025-04-29 23:39:39 +0200

    add Postgresql Configuraion

--- src/test/java/fr/univrouen/rss25SB/Rss25SbApplicationTests.java
+++ src/test/java/fr/univrouen/rss25SB/Rss25SbApplicationTests.java
@@ -3,7 +3,9 @@ package fr.univrouen.rss25SB;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class Rss25SbApplicationTests {
 
 	@Test
