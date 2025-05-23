6ea072710f89e09328d03eb96b5d9968c0b7d4c9
sadyanis <126914554+sadyanis@users.noreply.github.com>
2025-05-23 22:10:30 +0200

    add controleur

--- src/main/java/fr/univrouen/rss25SB/controllers/StdController.java
+++ src/main/java/fr/univrouen/rss25SB/controllers/StdController.java
@@ -6,11 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StdController {
+    @Autowired
+    private ItemRepository ItemRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "Accueil");
+        return "index";
+    }
+
+    @GetMapping("/help")
+    public String help(Model model) {
+        return "help";
+    }
+
+    @GetMapping("/rss25SB/resume/html")
+    public String resume(Model model) {
+        List<Item> items = ItemRepository.findAll();
+        model.addAttribute("items", items);
     @Autowired
     private ItemRepository ItemRepository;
     @GetMapping("/")
@@ -27,6 +47,19 @@ public class StdController {
         List<Item> items = ItemRepository.findAll();
         model.addAttribute("items", items);
 
+        return "resumeHtml";
+    }
+
+    @GetMapping("/rss25SB/html/{id}")
+    public String article(Model model, @PathVariable Long id) {
+        Optional<Item> optionalItem = ItemRepository.findById(id);
+        if (optionalItem.isPresent()) {
+            model.addAttribute("item", optionalItem.get());
+            return "articleHtml";
+        } else {
+            return "itemNotFound"; // Crée une vue "itemNotFound.html" pour gérer l'erreur proprement
+        }
+    }
         return "resumeHtml";
     }
 
